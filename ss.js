5dc8acdfd2a4296608e3a5d4185044394eb7c281
riku-pramanik-2003 <154338949+riku-pramanik-2003@users.noreply.github.com>
2024-04-03 19:52:09 +0530

    added index.html
    Signed-off-by: riku-pramanik-2003 <154338949+riku-pramanik-2003@users.noreply.github.com>

---
+++ ss.js
@@ -0,0 +1,22 @@
+//CALCULATER PROGRAM 
+   
+ const display = document.getElementById("display");
+ 
+ function appendToDisplay(input){
+    display.value += input;
+
+ }
+
+ function caearDisplay (){
+display.value = "";
+ }
+
+ function calculate(){
+try{
+    display.value = eval(display.value);
+}
+catch(error)
+{
+    display.value ="error";
+}
+ }
